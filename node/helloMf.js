c211cb2ad2afd35da42f67612d596ac4f661729a
BCD <bdimperio@mortgageflex.com>
2013-08-08 11:11:29 -0400

    working on auth
    TypeError: Cannot read property 'mycookiekey' of undefined
        at Object.cookieSession [as handle] (C:\projects\javascript\node\node_modules\express\node_modules\connect\lib\middleware\cookieSession.js:70:34)

--- node/helloMf.js
+++ node/helloMf.js
@@ -3,18 +3,52 @@
 var express = require('express'),
     app = express(),
     fs=require('fs');
-
+    
+function checkAuth(req, res, next) { //http://stackoverflow.com/a/8003291/57883
+  console.log('checking auth!');
+  if (!req.session.user_id) {
+    res.send('You are not authorized to view this page');
+  } else {
+  	res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+  }
+}
 app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.cookieSession({secret:'mysecret',key:'mycookiekey'}));
 
 app.get('/', function(req, res){
 	var path= "..\\..\\publish.ang.htm";
 	fs.readFile(path,function(err,data){
 		res.type('html');
 		res.send(data);
-	})
-    
-
+	});
 });
+app.get('/my_secret_page', checkAuth, function (req, res) {
+  res.send('if you are viewing this page it means you are logged in');
+});
+app.post('/login', function (req, res) {
+	if(!req.body){
+		res.send('no body');
+	} else {
+		var post = req.body;
+  		if (post.user == 'john' && post.password == 'johnspassword') {
+    		req.session.user_id = post.user;
+    		res.redirect('/my_secret_page');
+  		} else {
+    		res.send('Bad user/pass');
+  		}		
+	}
+  
+});
+app.get('/login',function(req,res){
+	var path= "..\\html\\login.htm";
+	fs.readFile(path,function(err,data){
+		res.type('html');
+		res.send(data);
+	});
+});
+
 
 if(!process.env){
 	process.env={};
