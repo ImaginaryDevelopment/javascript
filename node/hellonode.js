6e323af1a3dcc878fd940ab8bea7b6db09cd16bc
BCD <bdimperio@mortgageflex.com>
2013-08-08 15:03:45 -0400

    updating to work in regular node (outside cloud9)

--- node/hellonode.js
+++ node/hellonode.js
@@ -1,5 +1,13 @@
 //https://docs.c9.io/writing_nodejs_hello_world.html
 var http = require('http');
+// for node without c9:
+if(!process.env){
+	process.env={};
+}
+if(!process.env.PORT){
+	process.env.PORT=81;
+}
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello Node\n');
