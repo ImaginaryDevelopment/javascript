e3c952b01e1a38d9949e1aa778dcc2e9e75f0f72
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-01-17 14:05:08 -0500

    moar karma tests

--- karma/tests/angelloModelSpec.js
+++ karma/tests/angelloModelSpec.js
@@ -1,20 +1,43 @@
 describe('Service: angelloModel', function() {
-		// load the service's module    
-		beforeEach(module('Angello'));
-		var modelService;
-		// Initialize the service    
-		beforeEach(inject(function(angelloModel) {
-			modelService = angelloModel;
-		}));
-		describe('#getStatuses', function() { // write tests for angelloModel.getStatuses here    }); });
-				it('should return seven different statuses', function() {
-					expect(modelService.getStatuses().length).toBe(7);
-				});
-				it('should have a status named "To Do"', function() {
-						expect(modelService.getStatuses().map(function(status) { // get just the name of each status   
-								return status.name;
-							})).toContain('To Do');
-						});
-
-				});
+	// load the service's module    
+	beforeEach(module('Angello'));
+	var modelService;
+	// Initialize the service    
+	beforeEach(inject(function(angelloModel) {
+		modelService = angelloModel;
+	}));
+	describe('#getStatuses', function() { // write tests for angelloModel.getStatuses here    }); });
+		it('should return seven different statuses', function() {
+			expect(modelService.getStatuses().length).toBe(7);
 		});
+		it('should have a status named "To Do"', function() {
+			expect(modelService.getStatuses().map(function(status) { // get just the name of each status   
+				return status.name;
+			})).toContain('To Do');
+		});
+
+	});
+	describe('#getTypes', function() {
+		it('should return four different types', function() {
+			expect(modelService.getTypes().length).toBe(4);
+		});
+		it('should have a type named "Bug"', function() {
+			expect(modelService.getTypes().map(function(status) { // get just the name of each status         
+				return status.name;
+			})).toContain('Bug');
+		});
+	});
+
+	describe('#getStories', function() {
+		it('should return six different stories', function() {
+			expect(modelService.getStories().length).toBe(6);
+		});
+		it('should return stories that have a description property', function() {
+			modelService.getStories().forEach(function(story) {
+				expect(story.description).toBeDefined();
+			});
+		});
+	});
+
+
+});
