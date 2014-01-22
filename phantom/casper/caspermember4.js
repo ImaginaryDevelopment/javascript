500f20a20392726ce54b6e87c115476231d4e29e
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-01-22 17:07:47 -0500

    works with multiple sites and multiple pages, still no description nesting

--- phantom/casper/caspermember4.js
+++ phantom/casper/caspermember4.js
@@ -1,32 +1,79 @@
 // googletesting.js
-casper.test.begin('Cvs4 site', 4, function suite(test) {
-	var bases=['http://localhost:17054','http://dev.cvsresearch.com:17054'];
-	var everyPageTests=function(){
-		test.assertTextDoesntExist('Server Error');
-	};
-	casper.start(bases[0], function() {
-		everyPageTests();
-		test.assertTitle("Select a panel", "homepage title is the one expected");
-		
-		/*this.fill('form[action="/search"]', {
+var bases = [{
+	host: 'http://localhost:17054',
+	name: 'localhost'
+}, {
+	host: 'http://dev.cvsresearch.com:17054',
+	name: 'local as dev.cvsresearch.com'
+}];
+var everyPageTests = function(test, expectedUrl, actualUrl) {
+	var currentUrl = expectedUrl + ',' + actualUrl;
+	if (expectedUrl === actualUrl) {
+		currentUrl = actualUrl;
+	}
+
+	test.assertTextDoesntExist('Server Error', 'server error in '+currentUrl);
+};
+var testSite = function(base) {
+	casper.test.begin(base.name, 5, function suite(test) {
+
+
+		casper.start(base.host, function() {
+			var getCurrentUrl = casper.getCurrentUrl();
+			everyPageTests(test, base.host, getCurrentUrl);
+			test.assertTitle("Select a panel", "homepage title is the one expected");
+
+			/*this.fill('form[action="/search"]', {
             q: "casperjs"
         }, true); */
-	});
-	casper.thenOpen('http://localhost:17054/surveys', function() {
-		everyPageTests();
-		test.assertExists('.site-img-logo', "main logo is found");
+		});
+		var testOpenPage = function(url, test) {
+			casper.thenOpen(url, function() {
+				var currentUrl = casper.getCurrentUrl();
+				everyPageTests(test, url, currentUrl);
+			});
+		};
 
-	});
-	casper.thenOpen(bases[0]+'/profiles',function(){
-
-	});
-
-	/*
+		var urlTarget = base.host + '/surveys/';
+		casper.thenOpen(urlTarget, function() {
+			var getCurrentUrl = casper.getCurrentUrl();
+			everyPageTests(test, urlTarget, getCurrentUrl);
+			test.assertExists('.site-img-logo', "main logo is found");
+		});
+		testOpenPage(base.host + '/surveys/history/', test);
+		testOpenPage(base.host+'/surveys/polls/',test);
+		testOpenPage(base.host+'/profiles/',test);
+		/*
+		casper.thenOpen(base.host + '/Surveys/Polls', function() {
+			var getCurrentUrl = getCurrentUrl();
+			everyPageTests(test, getCurrentUrl);
+		})
+		casper.thenOpen(base.host + '/profiles', function() {
+			var getCurrentUrl = getCurrentUrl();
+			everyPageTests(test, getCurrentUrl);
+		});
+*/
+		/*
 	casper.then(function() {
 
 	});
 */
+		casper.run(function() {
+			test.done();
+		});
+	});
+};
+for (var i = bases.length - 1; i >= 0; i--) {
+	testSite(bases[i]);
+};
+
+/*
+casper.test.begin('another site', 1, function suite(test) {
+	casper.start(bases[1].host, function() {
+		everyPageTests(test);
+	})
 	casper.run(function() {
 		test.done();
 	});
 });
+*/
