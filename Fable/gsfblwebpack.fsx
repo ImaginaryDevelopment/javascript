474c97bc315b60fe11a500c0891fadcb2a22e3a1
bdimpe@gmail <bdimpe@gmail>
2016-09-13 11:48:56 -0400

    browser output is working, initial check-in before clean-up attempt

--- Fable/gsfblwebpack.fsx
+++ Fable/gsfblwebpack.fsx
@@ -1,10 +1,11 @@
 ﻿// http://kcieslak.io/Getting-Started-with-Fable-and-Webpack
-#r "./node_modules/fable-core/Fable.Core.dll"
+#r "node_modules/fable-core/Fable.Core.dll"
 
 open Fable.Core 
 open Fable.Import
+open Fable.Import.Browser
  
-Node.require.Invoke("core-js") |> ignore
+//Node.require.Invoke("core-js") |> ignore
 
 let element = Browser.document.getElementById "sample"
 element.innerText <- "Hello, world !!"
---
+++ Fable/installnpmdeps.bat
@@ -0,0 +1,11 @@
+﻿rem this was all required it seems
+npm init
+npm install --save-dev fable-compiler
+npm install --save-dev webpack
+npm install --save-dev source-map-loader
+npm install --save core-js
+npm install --save fable-core
+rem apparently this could be avoided if the fableconfig.json specifies 
+rem postbuild: node node_modules/webpack/bin/webpack
+npm install -g webpack
+fable
